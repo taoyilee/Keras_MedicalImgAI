7be3eecc4e6b25548e2739c2f59f9c668d91abbb
Michael (Tao-Yi) Lee <tylee@mlpanda.rocks>
2018-02-06 21:35:52 +0800

    rename classes to avoid collision

--- data/gen_data_entry.py
+++ data/gen_data_entry.py
@@ -21,10 +21,10 @@ with open('image_data_entry.csv', 'w', newline='') as csvfile:
         img = cv.imread(imgdir + images )
         if fnmatch.fnmatch(images, 'ITFX_*.jpg'):
             print ('Intertrochanter fracture ' + images)
-            spamwriter.writerow([images, 'ITFX', patient_ID, img.shape[0], img.shape[1]])
+            spamwriter.writerow([images, 'Intertrochanteric_FX', patient_ID, img.shape[0], img.shape[1]])
         if fnmatch.fnmatch(images, 'FX_*.jpg'):
             print ('Femoral neck fracture ' + images)
-            spamwriter.writerow([images, 'FX', patient_ID, img.shape[0], img.shape[1]])
+            spamwriter.writerow([images, 'Femoral_Neck_FX', patient_ID, img.shape[0], img.shape[1]])
         if fnmatch.fnmatch(images, 'NM_*.jpg'):
             print ('Normal ' + images)
             spamwriter.writerow([images, 'Normal', patient_ID, img.shape[0], img.shape[1]])
