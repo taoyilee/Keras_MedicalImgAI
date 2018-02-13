5811df95b95e3b67e35a2ff10e62119797964b4b
Michael Lee <tylee@ieee.org>
2018-02-13 11:24:24 +0800

    add property methods to print image counts
    Former-commit-id: 6b1b8396f4135a0841ce3cc54adc9006c575f5ed

--- app/datasets/dataset_loader.py
+++ app/datasets/dataset_loader.py
@@ -10,8 +10,6 @@ from app.datasets.dataset_utility import label2vec, pos_count, DataSequence, get
 
 
 class DataSet:
-    total_patients = 0
-    total_images = 0
     train = []
     dev = []
     test = []
@@ -38,24 +36,44 @@ class DataSet:
         else:
             self.reload_dataset()
 
-        self.train_count = len(self.train)
-        self.dev_count = len(self.dev)
-        self.test_count = len(self.test)
-        self.train_pos_count = pos_count(self.train, self.class_names)
-        self.dev_pos_count = pos_count(self.dev, self.class_names)
-        self.test_pos_count = pos_count(self.test, self.class_names)
-        self.print_summary()
-        # export csv
         output_fields = ["Image Index", "Patient ID", "Finding Labels"]
         self.train[output_fields].to_csv(self.train_csv, index=False)
         self.dev[output_fields].to_csv(self.dev_csv, index=False)
         self.test[output_fields].to_csv(self.test_csv, index=False)
+        self.print_summary()
+
+    @property
+    def img_count(self, subdataset="all"):
+        return_dict = {"train": len(self.train), "dev": len(self.dev),
+                       "test": len(self.test),
+                       "all": self.img_count("train") + self.img_count("dev") + self.img_count("test")}
+        return return_dict[subdataset.lower()]
+
+    @property
+    def img_pos_count(self, subdataset="train"):
+        return_dict = {"train": pos_count(self.train, self.class_names), "dev": pos_count(self.train, self.class_names),
+                       "test": pos_count(self.train, self.class_names)}
+        return return_dict[subdataset.lower()]
+
+    @property
+    def pat_count(self, subdataset="all"):
+        return_dict = {"train": len(list(self.train["Patient ID"].unique())),
+                       "dev": len(list(self.dev["Patient ID"].unique())),
+                       "test": len(list(self.test["Patient ID"].unique())),
+                       "all": self.pat_count("train") + self.pat_count("dev") + self.pat_count("test")}
+        return return_dict[subdataset.lower()]
 
     def print_summary(self):
-        print(
-            f"Total patients = {self.total_patients} in train/dev/test {train_patient_count}/{dev_patient_count}/{test_patient_count}")
-        print(
-            f"Total images = {self.total_images} in train/dev/test {self.train_count}/{self.dev_count}/{self.test_count}")
+        print(f'Total patients = {self.pat_count(subdataset="all")} ', end="")
+        print('in train/dev/test ', end="")
+        print(f'{self.pat_count(subdataset="train")}/', end="")
+        print(f'{self.pat_count(subdataset="dev")}/', end="")
+        print(f'{self.pat_count(subdataset="test")}')
+        print(f'Total images = {self.img_count(subdataset="all")}', end="")
+        print(f' in train/dev/test ', end="")
+        print(f'{self.img_count(subdataset="train")}/', end="")
+        print(f'{self.img_count(subdataset="dev")}/', end="")
+        print(f'{self.img_count(subdataset="test")}')
 
     def reload_dataset(self):
         print(f"Reloading splitted datasets from {self.train_csv}")
@@ -68,38 +86,26 @@ class DataSet:
         self.dev["One_Hot_Labels"] = self.dev["Finding Labels"].apply(lambda x: label2vec(x, self.class_names))
         self.test["One_Hot_Labels"] = self.test["Finding Labels"].apply(lambda x: label2vec(x, self.class_names))
 
-        pid_train = list(self.train["Patient ID"].unique())
-        pid_dev = list(self.dev["Patient ID"].unique())
-        pid_test = list(self.test["Patient ID"].unique())
-        train_patient_count = len(pid_train)
-        dev_patient_count = len(pid_dev)
-        test_patient_count = len(pid_test)
-
-        self.total_patients = train_patient_count + dev_patient_count + test_patient_count
-        self.total_images = len(self.train.index) + len(self.dev.index) + len(self.test.index)
-
     def split_dataset(self):
         print(f"Splitting dataset for {self.class_mode}")
-        os.makedirs(self.output_dir, exist_ok=True)
-        e = pd.read_csv(self.data_entry)
+        os.makedirs(self.dsconfig.output_dir, exist_ok=True)
+        e = pd.read_csv(self.dsconfig.data_entry)
 
         # one hot encode
-        e["One_Hot_Labels"] = e["Finding Labels"].apply(lambda x: label2vec(x, self.class_names))
+        e["One_Hot_Labels"] = e["Finding Labels"].apply(lambda x: label2vec(x, self.dsconfig.class_names))
 
         # shuffle and split
         pid = list(e["Patient ID"].unique())
         total_patients = len(pid)
-        train_patient_count = int(total_patients * train_ratio / 100)
-        dev_patient_count = int(total_patients * dev_ratio / 100)
-        test_patient_count = total_patients - train_patient_count - dev_patient_count
+        train_patient_count = int(total_patients * self.dsconfig.train_ratio / 100)
+        dev_patient_count = int(total_patients * self.dsconfig.dev_ratio / 100)
 
-        random.seed(self.random_state)
+        random.seed(self.dsconfig.random_state)
         random.shuffle(pid)
 
         self.train = e[e["Patient ID"].isin(pid[:train_patient_count])]
         self.dev = e[e["Patient ID"].isin(pid[train_patient_count:train_patient_count + dev_patient_count])]
         self.test = e[e["Patient ID"].isin(pid[train_patient_count + dev_patient_count:])]
-        total_images = len(e)
 
     def class_weights(self):
         print(f"** {self.class_mode} class_weights **")
@@ -114,10 +120,10 @@ class DataSet:
 
         elif self.class_mode == 'multibinary':
             class_weight = get_class_weights_multibinary(
-                self.train_count,
-                self.train_pos_count,
-                multiply=self.positive_weights_multiply,
-                use_class_balancing=self.use_class_balancing)
+                self.img_count(subdataset="train"),
+                self.img_pos_count(subdataset="train"),
+                multiply=self.dsconfig.positive_weights_multiply,
+                use_class_balancing=self.dsconfig.use_class_balancing)
             for c, w in class_weight.items():
                 print(f"  {c}: {w}")
 
@@ -125,13 +131,15 @@ class DataSet:
 
     def train_generator(self, verbosity=0):
         batch = self.train.sample(frac=1)  # shuffle
-        return DataSequence(batch, image_dir=self.image_dir, set_name='train',
-                            img_dim=self.img_dim, scale=self.scale, class_mode=self.class_mode, verbosity=verbosity)
+        return DataSequence(batch, image_dir=self.dsconfig.image_dir, set_name='train',
+                            img_dim=self.dsconfig.ImageConfig.img_dim, scale=self.dsconfig.ImageConfig.scale,
+                            class_mode=self.dsconfig.class_mode, verbosity=verbosity)
 
     def dev_generator(self, verbosity=0):
         batch = self.dev.sample(frac=1)  # shuffle
-        return DataSequence(batch, image_dir=self.image_dir, set_name='dev',
-                            img_dim=self.img_dim, scale=self.scale, class_mode=self.class_mode, verbosity=verbosity)
+        return DataSequence(batch, image_dir=self.image_dir, set_name='dev', img_dim=self.dsconfig.ImageConfig.img_dim,
+                            scale=self.dsconfig.ImageConfig.scale, class_mode=self.dsconfig.class_mode,
+                            verbosity=verbosity)
 
 
 class DataSetTest:
