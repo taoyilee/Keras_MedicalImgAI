53e40f47ace41f0a856a7a7e839a03edb61ccf05
Tao-Yi Lee <tylee@mlpanda.rocks>
2018-02-14 23:30:00 +0800

    update to Keras 2.1.4
    Former-commit-id: 47466de7be891e11aee510eabf49f1cbf773767b

--- datasets/chexnet_ds/resize.py
+++ datasets/chexnet_ds/resize.py
@@ -3,11 +3,11 @@ import csv
 import fnmatch
 import os
 import threading
-
+from concurrent.futures import ThreadPoolExecutor
 import cv2 as cv
 import numpy as np
 
-from .resize_img import resize_img
+from resize_img import resize_img
 
 parser = argparse.ArgumentParser(description='Generate data entry from image dir')
 parser.add_argument('input_dir', metavar='output_dir', type=str, help='the output directory')
@@ -15,18 +15,14 @@ parser.add_argument('output_dir', metavar='output_dir', type=str, help='the inpu
 parser.add_argument('output_size', metavar='output_size', type=int, help='output_size')
 
 args = parser.parse_args()
+input_dir = str(args.input_dir)
 output_dir = str(args.output_dir)
 output_size = args.output_size
-prefix_str = ['FNFX', 'ITFX', 'NM']
-patient_ID = 1
 print(f"Resizing all images to {output_size}")
 
 os.makedirs(output_dir, exist_ok=True)
-with open('image_data_entry.csv', 'w', newline='') as csvfile:
-    csvwriter = csv.writer(csvfile, delimiter=',', quotechar='|', quoting=csv.QUOTE_MINIMAL)
-    csvwriter.writerow(['Image Index', 'Finding Labels', 'Patient ID', 'OriginalImage Width', 'OriginalImage Height'])
-
-    for images in os.listdir(path=v):
-        image_path = os.path.join(v, images)
-        threading.Thread(target=resize_img, args=(csvwriter, image_path, output_dir, output_size)).start()
-        patient_ID = patient_ID + 1
+with ThreadPoolExecutor(max_workers=64) as executor:
+    for images in os.listdir(path=input_dir):
+        image_path = os.path.join(input_dir, images)
+        args = (image_path, output_dir, output_size)
+        executor.submit(resize_img, *args)
