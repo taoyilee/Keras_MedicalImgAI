765827a17ff987867d8fb609d3ae3bfc0f92c8de
Michael Lee <tylee@ieee.org>
2018-02-15 17:17:02 +0800

    remove run instance parameter
    Former-commit-id: 173e94ee5b2eb6ebc70a7a5c9346062f55f15656

--- app/main/Trainer.py
+++ app/main/Trainer.py
@@ -180,7 +180,7 @@ class Trainer:
                 self.checkpoint,
                 TensorBoard(
                     log_dir=os.path.join(self.conf.output_dir, "logs", "run{}".format(self.training_stats["run"])),
-                    batch_size=self.conf.batch_size, histogram_freq=0, write_graph=True,
+                    batch_size=self.conf.batch_size, histogram_freq=1, write_graph=True,
                     write_grads=True, write_images=True, embeddings_freq=1),
                 ReduceLROnPlateau(monitor='val_loss', factor=0.1, patience=self.conf.patience_reduce_lr, verbose=1),
                 self.auroc,
